e17706a6aaedebb40be77a438bb3b35aace6fc9a
IsenRom <isenev02@mail.ru>
2020-12-16 20:59:11 +0700

    multi

--- Lab4.2/Program.cs
+++ Lab4.2/Program.cs
@@ -4,6 +4,10 @@ namespace Lab4._2
 {
     class Program
     {
+        static int multi(int a, int b)
+        {
+            return a * b;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");

84a01bbe311336e9af8901d641d49dd8579aeb49
IsenRom <isenev02@mail.ru>
2020-12-16 20:57:56 +0700

    plus

--- Lab4.2/Program.cs
+++ Lab4.2/Program.cs
@@ -4,6 +4,10 @@ namespace Lab4._2
 {
     class Program
     {
+        static int plus(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");

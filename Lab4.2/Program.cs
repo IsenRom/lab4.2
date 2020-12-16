04feb4e1a61ebdcc9fe05df09b7bae9ca2cb5fb0
IsenRom <isenev02@mail.ru>
2020-12-16 21:05:19 +0700

    add minus

--- Lab4.2/Program.cs
+++ Lab4.2/Program.cs
@@ -12,6 +12,10 @@ namespace Lab4._2
         {
             return a / b;
         }
+        static int minus(int a, int b)
+        {
+            return a - b;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
